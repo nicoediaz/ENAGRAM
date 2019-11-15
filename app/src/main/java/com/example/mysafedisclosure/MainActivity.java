6cf4cd72fd8d4cef90e53bb463b84df2ade16b52
Nicolas Emilio Diaz Ferreyra <nicolas.diaz-ferreyra@uni-due.de>
2019-11-15 11:38:34 +0100

    First commit

--- app/src/main/java/com/example/mysafedisclosure/MainActivity.java
+++ app/src/main/java/com/example/mysafedisclosure/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//This is a new comment
+        setContentView(R.layout.activity_main);//This is a new comment...
 
 
         Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);//Reference to the second activity button
